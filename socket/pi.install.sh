c5796d2d23faf0438a0de4234e39281cb43745ff
MatthieuLemoine <ml.94230@gmail.com>
2016-01-05 21:49:10 +0100

    Fix pi install

--- socket/pi.install.sh
+++ socket/pi.install.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 
-sudo apt-get install git
-cd /tmp && wget https://nodejs.org/dist/latest-v5.x/node-v5.3.0-linux-armv6l.tar.gz;
-tar -xzf node-v5.3.0-linux-armv6l.tar.gz;
-sudo rm node-v5.3.0-linux-armv6l.tar.gz;
-sudo rm -R /opt/nodejs/node-v5.3.0-linux-armv6l/;
-sudo mv node-v5.3.0-linux-armv6l/ /opt/nodejs/;
-sudo rm /usr/bin/node /usr/sbin/node /sbin/node /usr/local/bin/node /usr/bin/npm /usr/sbin/npm /sbin/npm /usr/local/bin/npm;
-sudo ln -s /opt/nodejs/bin/node /usr/bin/node;
-sudo ln -s /opt/nodejs/bin/node /usr/sbin/node;
-sudo ln -s /opt/nodejs/bin/node /sbin/node;
-sudo ln -s /opt/nodejs/bin/node /usr/local/bin/node;
-sudo ln -s /opt/nodejs/bin/npm /usr/bin/npm;
-sudo ln -s /opt/nodejs/bin/npm /usr/sbin/npm;
-sudo ln -s /opt/nodejs/bin/npm /sbin/npm;
-sudo ln -s /opt/nodejs/bin/npm /usr/local/bin/npm;
+cd /tmp && wget https://nodejs.org/dist/latest-v4.x/node-v4.2.4-linux-armv6l.tar.gz;
+tar -xzf node-v4.2.4-linux-armv6l.tar.gz;
+sudo rm node-v4.2.4-linux-armv6l.tar.gz;
+cd node-v4.2.4-linux-armv6l/;
+rm -f /usr/local/bin/node /usr/local/bin/npm;
+sudo cp bin/node /usr/local/bin/node;
+sudo cp -R lib/node_modules /usr/local/lib/;
+sudo ln -s /usr/local/lib/node_modules/npm/bin/npm-cli.js /usr/local/bin/npm;
