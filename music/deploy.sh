c171b7f39da40421150806d1e1c7ff35b00c3e15
MatthieuLemoine <ml.94230@gmail.com>
2016-01-26 23:55:44 +0100

    Update deploy script

--- music/deploy.sh
+++ music/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 ssh pi@music.connected.house 'cd ~/connected-house && git pull origin master';
-scp -r build/ css/ pi@music.connected.house:~/connected-house/music;
+scp -r public/build/ public/css/ pi@music.connected.house:~/connected-house/music/public/;
